a89583e8731dd4e67f370c9ee25a221298be343e
Olga <nimfaolya@mail.ru>
2019-10-25 10:41:17 +0200

    addLoginForm

--- SystemUdviklingH5/SystemUdviklingH5/Employee/LoginForm.cs
+++ SystemUdviklingH5/SystemUdviklingH5/Employee/LoginForm.cs
@@ -6,5 +6,26 @@ namespace SystemUdviklingH5.Employee
 {
     class LoginForm
     {
+        private string employeeId;
+        private string password;
+
+        public LoginForm()
+        {
+            this.employeeId = "Admin";
+            this.password = "Password1";
+        }
+
+        public void Login(string employeeId, string password)
+        {
+
+        }
+
+        public void DisplayResetPasswordForm(string password)
+        {
+
+        }
+
+
+        
     }
 }
